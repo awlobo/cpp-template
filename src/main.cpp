be2199866d04b11abccc644a41f9d00d332f47ae
Andrea Lobo <andrea.lobo.arroyo@gmail.com>
2021-04-25 22:52:04 +0200

    Add template test

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
+#define DOCTEST_CONFIG_IMPLEMENT
+#include "../tests/doctest.h"
 #include <iostream>
 
-int main()
-{
+int main(int argc, char** argv) {
     std::cout << "Hello World!" << std::endl;
+    doctest::Context context;
+    context.applyCommandLine(argc, argv);
+    return context.run();
 }
